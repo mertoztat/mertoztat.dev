804133a7cc3c1819fb7f278cbd3c8a1dc5dc8ffe
Mert Öztat <mertoztat@gmail.com>
2022-10-16 22:29:24 +0300

    Structures created & navbar-personal page done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => setClicked(!clicked);
+  // bg-[#1f2020]
+  return (
+    <div className="fixed w-full h-[80px] bg-red-800  text-gray-300 flex items-center justify-between px-5">
+      Logo
+      <ul className="hidden md:flex">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contacts</li>
+      </ul>
+      {/* Hamburger Menu */}
+      <div className="cursor-pointer md:hidden z-20" onClick={handleClick}>
+        {clicked ? <FaTimes /> : <FaBars />}
+      </div>
+      {/* Mobile Menu */}
+      <ul
+        className={
+          clicked
+            ? "absolute top-0 left-0 flex flex-col text-gray-300 h-screen w-screen bg-[#1f2020] items-center justify-center"
+            : "hidden"
+        }
+      >
+        <li className="py-4 text-2xl items-center justify-center hover:scale-150 transition duration-300 ease-in-out ">
+          Home
+        </li>
+        <li className="py-4 text-2xl items-center justify-center hover:scale-150  transition duration-300 ease-in-out ">
+          About
+        </li>
+        <li className="py-4 text-2xl items-center justify-center hover:scale-150  transition duration-300 ease-in-out">
+          Skills
+        </li>
+        <li className="py-4 text-2xl items-center justify-center hover:scale-150 transition duration-300 ease-in-out ">
+          Projects
+        </li>
+        <li className="py-4 text-2xl items-center justify-center hover:scale-150  transition duration-300 ease-in-out">
+          Contacts
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Personal.jsx
@@ -0,0 +1,39 @@
+import Mert from "../assets/mert.png";
+import { TypeAnimation } from "react-type-animation";
+
+const Personal = () => {
+  return (
+    <div className="w-full h-screen bg-[#1f2020]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-screen">
+        <div className="grid sm:grid-cols-2 gap-4">
+          <img src={Mert} alt="mertöztat" />
+          <div className="flex flex-col">
+            <h1 className="text-2xl sm:text-6xl font-medium  py-4 text-red-300 text-center sm:text-left sm:pt-28 ">
+              Hi, I'm Mert.
+            </h1>
+            <div
+              id="intro"
+              className=" text-red-500 justify-center  flex items-center gap-2 py-4 text-center sm:justify-start"
+            >
+              <TypeAnimation
+                sequence={[
+                  "<Frontend Developer/>",
+                  1500, //
+                  "My goal is Fullstack Developer ",
+                  750,
+                ]}
+                wrapper="div"
+                cursor={false}
+                repeat={Infinity}
+                style={{ fontSize: "24px", maxWidth: "500px" }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Personal;
