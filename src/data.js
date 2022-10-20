9a8867cdcbc3308adf58e319d49eeb0162b34ff9
Mert Öztat <mertoztat@gmail.com>
2022-10-20 20:20:13 +0300

    Data created

---
+++ src/data.js
@@ -0,0 +1,56 @@
+import css from "../src/assets/skills/css.png";
+import html from "../src/assets/skills/html.png";
+import javascript from "../src/assets/skills/javascript.png";
+import bootstrap from "../src/assets/skills/bootstrap.png";
+import firebase from "../src/assets/skills/firebase.png";
+import git from "../src/assets/skills/git-logo.png";
+import react from "../src/assets/skills/react.png";
+import sass from "../src/assets/skills/sass2.png";
+import tailwind from "../src/assets/skills/tailwind.png";
+import next from "../src/assets/skills/next.png";
+
+export const skillsImages = [
+  html,
+  css,
+  javascript,
+  react,
+  next,
+  sass,
+  tailwind,
+  bootstrap,
+  firebase,
+  git,
+];
+
+export const projects = [
+  {
+    id: 1,
+    img: "",
+    name: "Sneakers E-Commerce",
+    description:
+      "In this project was case to FrontendMentor.io. I made responsive, functionality, e commerce product detail page. This app have add, update & delete cart. ",
+    tech: "React JS, Context API, React Hooks",
+    demo: "",
+    github: "",
+  },
+  {
+    id: 2,
+    img: "",
+    name: "Pokemon Memory Card",
+    description:
+      "A memory game has been made where we try to find matching cards with using ",
+    tech: "React JS, React Hooks",
+    demo: "",
+    github: "",
+  },
+  {
+    id: 3,
+    img: "",
+    name: "Unsplash Search Photo Gallery",
+    description:
+      "In this project i made it like unsplash clone. I use unsplash api, tailwindcss & reactjs",
+    tech: "React JS, React Hooks, Tailwind CSS, Unsplash Api",
+    demo: "",
+    github: "",
+  },
+];
